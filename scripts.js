120237cc2ecedb31ebafcae4d559b850a166c82a
Grzegorz Szermiński <szerman@poczta.fm>
2018-02-07 17:08:00 +0100

    Change js content

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-var text= 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
 console.log(text);
 var dinosaur = 'Triceratops';
 var dinosaurUpperCase = dinosaur.toUpperCase();
 var textChange = text.replace('Velociraptor', dinosaurUpperCase);
-console.log('Half of the textChange is on the ' + textChange.length/2 + ' character.');
-console.log(textChange.slice(0,72));
+//console.log('Half of the textChange is on the ' + textChange.length/2 + ' character.');
+//console.log(textChange.slice(0,72));
+console.log(textChange.length/2); 
